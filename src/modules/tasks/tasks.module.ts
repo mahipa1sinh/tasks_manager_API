c332ed8874a78ec4dd9ed857e44b32e709877f04
Mahipalsinhnakum <nakummahipalsinh007@gmail.com>
2023-02-07 13:45:48 +0530

    change

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, NestModule } from '@nestjs/common/interfaces';
 import { DatabaseModule } from 'src/db/database.module';
 import { TaskController } from './tasks.controller';
 import { TaskProvider } from './tasks.provider';
